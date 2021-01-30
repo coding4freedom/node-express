0f3c184e35a4d539bfb4df95862c488d57980bf0
Chris <chawkins2010@hotmail.com>
2021-01-30 12:35:37 -0600

    Week 1 workshop assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,5 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Context-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsites: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
 
 module.exports = campsiteRouter;
